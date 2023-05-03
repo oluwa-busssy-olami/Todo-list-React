bf2de2276d5821887cad6ee67821aa1651367259
Busolami <oluwabibusola@gmail.com>
2023-05-03 16:17:33 +0100

    updated the Todolist

--- src/Component/FormSection.js
+++ src/Component/FormSection.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 const FormSection = () => {
+  const [newTask, setNewTask] = useState();
+  const handleChange = () => {};
   return (
     <div className="newtask">
-      <input type="text" placeholder="Add a task" />
+      <input
+        type="text"
+        placeholder="Add a task"
+        value={newTask}
+        onChange={handleChange}
+      />{" "}
       <button className="push"> Add </button>{" "}
     </div>
   );
